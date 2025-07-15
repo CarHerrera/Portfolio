7708a5cb5629a5faa1970ba679c49c9ba8679f67
Carlos <herrera4345@gmail.com>
2025-07-15 17:23:21 -0400

    Better Nav Bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,54 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+import Link from 'next/link'
+export default function Page(){
+    return (
+        <div>
+            <div className="flex-row hidden grid grid-cols-2 bg-zinc-800 text-white md:flex">
+                <div className="flex-grow px-8 py-8">Carlos Herrera</div>
+                <div className= "flex-none flex-row right-full place-content-end">
+                    <Link className="float-right px-8 py-8 hover:bg-blue-100" href="About">About</Link>
+                    <a className="float-right px-8 py-8 hover:bg-blue-100">Projects</a>
+                    <a className="float-right  px-8 py-8 hover:bg-blue-100">Experience</a>
+                    <a className="float-right  px-8 py-8 hover:bg-blue-100">Contact Me</a>
+                </div>
+            </div>
+            <div className="hidden h-1/2 grid-cols-2 bg-gray-700 p-10 text-white md:grid">
+            <div className="px-5 py-10">
+                This is text
+                <br />
+                <br />
+                This is more text describing me I guess and some of my specialties
+                <br />
+                <br />
+                <div className="flex flex-row">Wooooooooooooooooooooooooooow</div>
+                </div>
+                <div>
+                <img className="rounded-full px-5 py-10" src="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg" />
+                </div>
+            </div>
+              <div className="hidden grid-cols-2 grid-rows-2 bg-black text-white md:grid">
+    
+    <div className="bg-gray-500 m-8 rounded-lg">
+        <h2>Project 1</h2>
+        <br/>
+        This is a project description that each project will eventually have (each one different obv).
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="bg-gray-500 m-8 rounded-lg">
+        <h2>Project 2</h2>
+        <br/>
+        This is a project description that each project will eventually have (each one different obv).
+        </div>
+        <div className="bg-gray-500 m-8 rounded-lg">
+        <h2>Project 3</h2>
+        <br/>
+        This is a project description that each project will eventually have (each one different obv).
+        </div>
+        <div className="bg-gray-500 m-8 rounded-lg">
+        <h2>Project 4</h2>
+        <br/>
+        This is a project description that each project will eventually have (each one different obv).
     </div>
-  );
+  </div>
+        </div>
+
+    );
 }
